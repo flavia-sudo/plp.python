baa98d8ff899d5d1d374d17a1167a531523a4019
flavia-sudo <kihahuflavia@gmail.com>
2024-03-13 10:43:06 +0300

    task 1

---
+++ python_first/user_input.py
@@ -0,0 +1,4 @@
+name = str(input("Enter user name "))
+age = int(input("Enter user age "))
+location = str(input("Enter user location "))
+print("Hello", name, "you are", age, "years old and live in", location)
